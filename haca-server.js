96be405f88c02446e46469f4d001aa5a1b33fa89
Tunna Duong <tunnaduong@gmail.com>
2022-11-10 11:28:14 +0700

    Updated for deployment

--- haca-server.js
+++ haca-server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const api = {};
 var server_port = 10070;
-var server_host = "localhost";
+var server_host = "103.81.85.224";
 server.listen(server_port, server_host, function () {
   console.log("Haca Live Music Server đang chạy tại cổng: %d", server_port);
 });
